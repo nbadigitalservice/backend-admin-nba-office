7124e28a65fa3a6d46a449dbba05f93e3587e2d6
Bew2000 <146051593+Bew2000@users.noreply.github.com>
2023-11-08 16:44:41 +0700

    Edit

--- controller/timesheet.controller.js
+++ controller/timesheet.controller.js
@@ -56,31 +56,27 @@ module.exports.CreateCheckin = async (req, res) => {
 
           const FullTime =(convert_getHours+":"+convert_getMin+":"+convert_getSecond);
           const FullDate = (getYear+"-"+convert_getMonth+"-"+convert_getDay);
-          
-          console.log("FullTime :", FullTime);
-          console.log("FullDate :", FullDate);
-        const getUserId = await User.findOne({name: req.user.name}).select("_id");
 
-        const chk_checkin = await Timesheet.find({$and:[{workDate: FullDate, name: req.user.name, userId: req.user.user_id}]});
+          const getUserId = await User.findOne({name: req.user.name}).select("_id");
+
+          const chk_checkin = await Timesheet.find({$and:[{workDate: FullDate, name: req.user.name, userId: req.user.user_id}]});
           console.log("chk_checkin>>>>>>", chk_checkin)
-        if(chk_checkin.length > 0){
-            return res.status(200).send({message: "คุณลงเวลาทำงานแล้ว"});
-        }else{
-            let CheckinTime = {
-                name: req.user.name,
-                workDate : FullDate,
-                checkin : FullTime,
-                checkout : "-",
-                total : "-",
-                ot : "-",
-                userId: req.user.user_id,
-            };
-            const nCheckin = new Timesheet(CheckinTime);
-            const Checkin = await nCheckin.save();
-            return res.status(200).send({message: "ลงเวลาทำงานสำเร็จ", data: Checkin});
+            if(chk_checkin.length > 0){
+                return res.status(200).send({message: "คุณลงเวลาทำงานแล้ว"});
+            }else{
+                let CheckinTime = {
+                    name: req.user.name,
+                    workDate : FullDate,
+                    checkin : FullTime,
+                    checkout : "-",
+                    total : "-",
+                    ot : "-",
+                    userId: req.user.user_id,
+                };
+                const nCheckin = new Timesheet(CheckinTime);
+                const Checkin = await nCheckin.save();
+                return res.status(200).send({message: "ลงเวลาทำงานสำเร็จ", data: Checkin});
         }
-
-       
     }catch(error){
         console.error(error);
         res.status(500).send({message: "Internal Server Error"});
@@ -94,10 +90,7 @@ module.exports.CreateCheckout = async (req, res) => {
         if (!permission.includes(req.user.level)) {
             return res.status(403).send({message: "Permission denied"});
           }
-        // var Datenow = new Date().toISOString('en-US', { timeZone: 'Asia/Jakarta' });
-        // var Today = Datenow.slice(0,10);
-        // var Time = Datenow.slice(11,19);
-          /* -------------------------------------- */
+
           const DateTime = new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' },
           {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit', second: "2-digit"});
           const getHours = new Date(DateTime).getHours();
@@ -132,10 +125,6 @@ module.exports.CreateCheckout = async (req, res) => {
             const TotalTime = await Timesheet.updateOne({name: req.user.name, workDate: FullDate}, {$set:{total: chkCalDate}})
             const reChk_checkout = await Timesheet.find({$and:[{workDate: FullDate,userId: req.user.user_id, name: req.user.name}]});
             
-            console.log("reChk_checkout", reChk_checkout[0]['checkout']);
-            console.log("workingStartCaltoSec", workingStartCaltoSec)
-            console.log("workingEndCaltoSec", workingEndCaltoSec);
-            console.log("TotalTime :", chkCalDate);
 
             const chkOT = await Timesheet.find({$and:[{workDate: FullDate,userId: req.user.user_id, name: req.user.name}]});
             if(chkOT[0]['checkout'] > "18:29:59"){
@@ -155,11 +144,6 @@ module.exports.CreateCheckout = async (req, res) => {
                     console.log("ot_sec", ot_sec);
                     console.log("caltoOT : ", caltoOT);
                     console.log("result_ot :", result_ot);
-
-
-
-                    
-                    /*--------------------------*/
             }else{
                     console.log("ยังไม่ถึงเวลา OT")
             }
@@ -211,31 +195,75 @@ module.exports.UpdateTimesheet = async (req, res) => {
   
         if (!permission.includes(req.user.level)) {
           return res.status(403).send({message: "Permission denied"});
-        }
+        }      const DateTime = new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' },
+        {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit', second: "2-digit"});
+        const getHours = new Date(DateTime).getHours();
+        const getMin  = new Date(DateTime).getMinutes();
+        const getSecond = new Date(DateTime).getSeconds();
+        var getDay = new Date(DateTime).getDate();
+        var getMonth = new Date(DateTime).getMonth()+1;
+        var getYear = new Date(DateTime).getFullYear();
+        var convert_getHours = getHours < 10 ? '0'+getHours : getHours;
+        var convert_getMin = getMin < 10 ? '0'+getMin : getMin;
+        var convert_getSecond = getSecond < 10 ? '0'+getSecond : getSecond;
+        var convert_getDay = getDay < 10 ? '0'+getDay : getDay;
+        var convert_getMonth = getMonth < 10 ? '0'+getMonth : getMonth;
+
+        const FullTime =(convert_getHours+":"+convert_getMin+":"+convert_getSecond);
+        const FullDate = (getYear+"-"+convert_getMonth+"-"+convert_getDay);
         const getWorkDate = req.body.workDate;
         const getCheckin = req.body.checkin;
         const getCheckout = req.body.checkout;
         const getTimesheetId = req.body.id;
-
+        const getUserId = await User.findOne({name: req.user.name}).select("_id");
         const workingStartToday = getCheckin.split(':');
         const workingEndToday = getCheckout.split(':');
         const workingStartCaltoSec = (+workingStartToday[0]) * 60 * 60 + (+workingStartToday[1]) * 60 + (+workingStartToday[2]);
         const workingEndCaltoSec = (+workingEndToday[0]) * 60 * 60 + (+workingEndToday[1]) * 60 + (+workingEndToday[2]);
         const MinusForSec = workingEndCaltoSec - workingStartCaltoSec ;
         const chkCalDate = ((MinusForSec/60)/60).toFixed(2);
-        console.log("MinusForSec", chkCalDate);
         const DataTimesheet = await Timesheet.find({userId: getUserId, workDate: getWorkDate});
+        console.log(req.user.user_id)
         const TimesheetUpdate = await Timesheet.updateMany(
             {_id: getTimesheetId},
             {$set: {checkin: getCheckin, checkout: getCheckout, total: chkCalDate}}
         );
-        
+        const getTimesheet = await Timesheet.find({$and:[{workDate: FullDate,userId: req.user.user_id}]});
+            const updateStartToday = (getTimesheet[0]['checkin']).split(':');
+            const updateEndToday = (getTimesheet[0]['checkout']).split(':');
+            const updateStartCaltoSec = (+updateStartToday[0]) * 60 * 60 + (+updateStartToday[1]) * 60 + (+updateStartToday[2]);
+            const updateEndCaltoSec = (+updateEndToday[0]) * 60 * 60 + (+updateEndToday[1]) * 60 + (+updateEndToday[2]);
+            const Minus_Sec = updateEndCaltoSec - updateStartCaltoSec ;
+            const chk_CalDate = ((Minus_Sec/60)/60).toFixed(2);
+            const TotalTime = await Timesheet.updateOne({name: req.user.name, workDate: FullDate}, {$set:{total: chk_CalDate}})
+            const reChk_checkout = await Timesheet.find({$and:[{workDate: FullDate,userId: req.user.user_id, name: req.user.name}]});
+            
+
+            const chkOT = await Timesheet.find({$and:[{workDate: FullDate,userId: req.user.user_id, name: req.user.name}]});
+            if(chkOT[0]['checkout'] > "18:29:59"){
+                    const start_ot = ("18:29:59").split(':');
+                    const time_ot = (chkOT[0]['checkout']).split(':');
+                    const ot_sec = (+time_ot[0]) * 60 * 60 + (+time_ot[1]) * 60 + (+time_ot[2]);
+                    const start_ot_sec = (+start_ot[0]) * 60 * 60 + (+start_ot[1]) * 60 + (+start_ot[2]);
+                    const caltoOT = (ot_sec - start_ot_sec) ;
+                    const result_ot = ((caltoOT/60)/60).toFixed(2);
+                    const calToMillianSec = (caltoOT*1000)
+                    const newDateOT = new Date(calToMillianSec);
+                    const timeOT = newDateOT.getHours()+":"+newDateOT.getMinutes();
+                    const TotalOT = await Timesheet.updateOne({name: req.user.name, workDate: FullDate}, {$set:{ot: result_ot}})
+                    const chkCalOT = "20:30:00" - "18:30:00";
+                    console.log("Time OT : ", timeOT);
+                    console.log("start sec", start_ot_sec);
+                    console.log("ot_sec", ot_sec);
+                    console.log("caltoOT : ", caltoOT);
+                    console.log("result_ot :", result_ot);
+            }else{
+                    console.log("ยังไม่ถึงเวลา OT")
+            }
 
 
-        console.log("getWorkDate", getWorkDate, "getCheckin", getCheckin, "getCheckout", getCheckout);
-        console.log("DataTimesheet >>> ", DataTimesheet);
-        // console.log("updateDataTimesheet", updateDataTimesheet);
-        return res.status(200).send({message: "Get Timesheet from date Success", DataTimesheet});
+
+        return res.status(200).send({message: "Get Timesheet from date Success"});
     }catch(error){
         return res.status(500).send({message: "Internal Server Error"});
     }
